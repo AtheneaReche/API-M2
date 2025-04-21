a0881c35329250b2e856178ecedd0eec6d78aa5d
AtheneaReche <atheneareche@gmail.com>
2025-04-21 19:38:19 +0200

    Update Publishers.

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { index } from '../controllers/indexController';
+import publishersRoutes from './publishers'
 
 const router = Router();
 
 router.get('/', index);
+router.use('/publishers', publishersRoutes);
 
 export default router;
--- app/routes/publishers.ts
+++ app/routes/publishers.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { getPublishers, createPublishers } from '../controllers/publishersCrler';
+import { getPublishers, getPublishersById, createPublishers, deletePublishers, updatePublishers } from '../controllers/publishersController';
 
 const router = Router();
-
+//Get that return all the Publishers list
 router.get('/', getPublishers);
+//Get that return specific Publisher by id
+router.get('/:id', getPublishersById);
+//Create Publishers
 router.post('/', createPublishers);
+//Delete Publishers by id
+router.delete('/:id', deletePublishers);
+//Update Publishers by id
+router.put('/:id', updatePublishers);
 
 export default router;
