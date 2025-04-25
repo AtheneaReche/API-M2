fc9f4f22884876611b4085768d50529357253d6f
AtheneaReche <atheneareche@gmail.com>
2025-04-25 20:56:18 +0200

    Add books GET query parameters to filter search

--- app/controllers/booksController.ts
+++ app/controllers/booksController.ts
@@ -4,7 +4,8 @@ import db from '../../database/db'
 //Get Method (all of them)
 export const getBooks = async (req: Request, res: Response): Promise<void> => {
     try{
-        const [rows] = await db.execute(`
+        const { author, publisher, genre } = req.query;
+        let sql = `
                 SELECT 
                 books.id,
                 books.book_cover,
@@ -17,25 +18,44 @@ export const getBooks = async (req: Request, res: Response): Promise<void> => {
             FROM books
             JOIN authors ON books.author = authors.id
             LEFT JOIN publishers ON books.publisher = publishers.id
-        `);
-        res.json(rows);
+        `;
+        const conditions: string[] = [];
+        const values: (string|number)[] = [];
+
+        if (author) {
+            conditions.push(`books.author = ?`);
+            values.push(Number(author));
+        }
+        if (publisher) {
+            conditions.push(`books.publisher = ?`);
+            values.push(Number(publisher));
+        }
+        if (genre){
+            conditions.push(`books.genre = ?`);
+            values.push(genre.toString());
+        }
+
+        if (conditions.length > 0) {
+            sql += ` WHERE ` + conditions.join(' AND ');
+        }
+
+        const [rows] = await db.execute(sql, values);
+        res.status(200).json(rows);
     } catch (err){
         console.error(err);
         res.status(500).send('Error fetching books ' + err)
     }
-    res.status(200).send();
 };
 //Get Method by Id
 export const getBooksById = async (req: Request, res: Response): Promise<void> => {
     try{
         const { id } = req.params;
         const book = await db.execute('SELECT * FROM books WHERE id = ?', [id]);
-        res.json(book);
+        res.status(200).json(book);
     } catch (err){
         console.error(err);
         res.status(500).send('Error fetching Books ' + err)
     }
-    res.status(200).send();
 };
 
 //Create Method
